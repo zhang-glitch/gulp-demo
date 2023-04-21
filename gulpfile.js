9f3a11cffa8eb6aea44fbd0e2c3c932e990d6ef1
zhang-glitch <669638556@qq.com>
2023-04-21 20:03:20 +0800

    [feat] 处理js文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const {src, dest} = require("gulp")
 // 安装sass,gulp-sass
 const sass = require('gulp-sass')(require('sass'));
+// babel只是一个转换平台，我们还需要安装对应的插件来时间对应语法的转化功能。
+// @babel/core @babel/preset-env
+const babel = require("gulp-babel")
 
 const style = () => {
   // 需要指定打包的base，来达到输出同样的目录
   return src("src/assets/styles/*.scss", {base: "src"}).pipe(sass()).pipe(dest("dist"))
 }
 
+const script = () => {
+  return src("src/assets/scripts/*.js", {base: "src"}).pipe(babel({presets: ["@babel/preset-env"]})).pipe(dest("dist"))
+}
+
 module.exports = {
-  style
+  style,
+  script
 }
